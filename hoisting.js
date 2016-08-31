7390b1f4cb5b674b0d04db59f6d0aefc68c8c5a9
Clifton Regis <github email address>
2016-08-31 09:00:14 -0400

    completed lab

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,9 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+   lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
+  
 }
 
 
@@ -12,19 +13,20 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+ function thisIsCrazy (){
     console.log("hey!!!")
   }
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
 
   function sayMy() {
+      var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+
+      var name = "Cricky";
+
   }
+  sayMy();
 }
